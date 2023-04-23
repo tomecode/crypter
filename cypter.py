0ed3c990cea12608665d4f71b1af9ea35465b9e1
Tome <tomecode@users.noreply.github.com>
2023-04-23 21:36:43 +0200

    Added support for mmap

--- cypter.py
+++ cypter.py
@@ -1,25 +1,49 @@
+import sys
+import os
+import mmap
 import asyncio
 from asyncio.queues import Queue
 
+def mmap_read_file_chunks(fh, size):
+    while True:
+        # file's object current position from the start
+        startat = fh.tell()
+        # offset from current postion -->1
+        fh.seek(size, 1)
+        yield startat, fh.tell() - startat
 
-async def read_input_file(input_file: str, input_queue: Queue):
-    with open(input_file, mode='r') as f:
-        while True:
-            chunk = f.readline()
-            if not chunk:
-                await input_queue.put(None)
-                input_queue.task_done()
-                break
-            await input_queue.put(chunk.strip())
+async def mmap_read_input_file(input_file: str, input_queue: Queue, chunk_size: int):
+    file_stats = os.stat(input_file)
+    if file_stats.st_size < chunk_size:
+        chunk_size = file_stats.st_size
+
+    with open(input_file, 'rb', ) as fh:
+        #read data with mmap
+        with mmap.mmap(fh.fileno(), length=0, access=mmap.ACCESS_COPY) as mmap_read:
+            for seek_from, seek_size in mmap_read_file_chunks(mmap_read, chunk_size):
+                # chunks start
+                mmap_read.seek(seek_from)
+                # chunk end
+                chunk = fh.read(seek_size)
+
+                if len(chunk) == 0:
+                    # no more chunks... stop p
+                    await input_queue.put(None)
+                    input_queue.task_done()
+                    break
+                    
+                await input_queue.put(chunk)
     return None
 
+#processing chunk
 async def encrypt(chunk):
     if chunk is None:
         return None
-    return ('e%s' % chunk)
+#    return ('e%s' % chunk)
+    return (chunk)
 
 async def process_chunk(input_queue: Queue, output_file: str, chunk_size: int):
-    with open(output_file, mode='w') as f:
+    with open(output_file, mode='wb') as f:
         while True:
             tasks = []
             for i in range (chunk_size):
@@ -33,7 +57,7 @@ async def process_chunk(input_queue: Queue, output_file: str, chunk_size: int):
                     print("processing chunks done")
                     input_queue.task_done()
                     return None
-                f.write(res + '\n')
+                f.write(res)
             results.clear()
             print("input_queue.qsize() %s" % input_queue.qsize())
             if input_queue.qsize() <= 1:
@@ -42,13 +66,13 @@ async def process_chunk(input_queue: Queue, output_file: str, chunk_size: int):
     return None
 
 async def main():
-    input_file = 'input.txt'
+    input_file = 'large_file'
     output_file = 'output.txt'
     chunk_size = 3
 
     input_queue = asyncio.Queue(maxsize=chunk_size)
 
-    input_task = asyncio.create_task(read_input_file(input_file, input_queue))
+    input_task = asyncio.create_task(mmap_read_input_file(input_file, input_queue, 10000000))
     process_task = asyncio.create_task(process_chunk(input_queue, output_file, chunk_size))
 
     await asyncio.gather(input_task, process_task)
