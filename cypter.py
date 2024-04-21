66603109a0710714e7877620720a64c2029b167b
Tome <tomecode@users.noreply.github.com>
2024-04-21 09:14:08 +0200

    Update cypter.py

--- cypter.py
+++ cypter.py
@@ -1,80 +1,97 @@
 import sys
 import os
 import mmap
+import time
 import asyncio
 from asyncio.queues import Queue
+
 
 def mmap_read_file_chunks(fh, size):
     while True:
         # file's object current position from the start
-        startat = fh.tell()
-        # offset from current postion -->1
+        start_pos = fh.tell()
+        # offset from current position -->1
         fh.seek(size, 1)
-        yield startat, fh.tell() - startat
+        yield start_pos, fh.tell() - start_pos
 
-async def mmap_read_input_file(input_file: str, input_queue: Queue, chunk_size: int):
+
+async def read_input_file(input_file: str, input_queue: Queue, file_data_chunk_size: int):
     file_stats = os.stat(input_file)
-    if file_stats.st_size < chunk_size:
-        chunk_size = file_stats.st_size
+    if file_stats.st_size < file_data_chunk_size:
+        file_data_chunk_size = file_stats.st_size
 
     with open(input_file, 'rb', ) as fh:
-        #read data with mmap
+        # read data with mmap
         with mmap.mmap(fh.fileno(), length=0, access=mmap.ACCESS_COPY) as mmap_read:
-            for seek_from, seek_size in mmap_read_file_chunks(mmap_read, chunk_size):
+            for seek_from, seek_size in mmap_read_file_chunks(mmap_read, file_data_chunk_size):
                 # chunks start
                 mmap_read.seek(seek_from)
                 # chunk end
                 chunk = fh.read(seek_size)
 
-                if len(chunk) == 0:
+                if chunk is None or len(chunk) == 0:
                     # no more chunks... stop p
                     await input_queue.put(None)
-                    input_queue.task_done()
                     break
-                    
+
                 await input_queue.put(chunk)
+
+    print("read done")
+    await input_queue.put(None)
     return None
 
-#processing chunk
-async def encrypt(chunk):
-    if chunk is None:
+
+# processing chunk from file
+def process_file_chunk(file_data_chunk):
+    if file_data_chunk is None:
         return None
-#    return ('e%s' % chunk)
-    return (chunk)
+    # TODO: implement process data, encrypt, send to db, etc.
+    return file_data_chunk
 
-async def process_chunk(input_queue: Queue, output_file: str, chunk_size: int):
-    with open(output_file, mode='wb') as f:
-        while True:
-            tasks = []
-            for i in range (chunk_size):
-                chunk = await input_queue.get()
-                if chunk is not None:
-                    tasks.append( asyncio.create_task(encrypt(chunk)) )
-            results = await asyncio.gather(*tasks)
 
-            for res in results:
-                if(res is None):
-                    print("processing chunks done")
-                    input_queue.task_done()
-                    return None
-                f.write(res)
-            results.clear()
-            print("input_queue.qsize() %s" % input_queue.qsize())
-            if input_queue.qsize() <= 1:
-                input_queue.task_done()
-                return None
+async def processor(input_queue: Queue, output_queue: Queue):
+    while True:
+        file_data_chunk = await input_queue.get()
+        if file_data_chunk is not None:
+            # print("More chunks to process")
+            await output_queue.put(process_file_chunk(file_data_chunk))
+        else:
+            break
+    print("processor done")
+    await output_queue.put(None)
     return None
 
-async def main():
-    input_file = 'large_file'
-    output_file = 'output.txt'
-    chunk_size = 3
 
-    input_queue = asyncio.Queue(maxsize=chunk_size)
+async def write_output_file(output_queue: Queue, output_file: str):
+    with open(output_file, "wb") as f:
+        while True:
+            file_data_chunk = await output_queue.get()
+            if file_data_chunk is not None:
+                f.write(file_data_chunk)
+            else:
+                break
+    print("write done")
+    return None
 
-    input_task = asyncio.create_task(mmap_read_input_file(input_file, input_queue, 10000000))
-    process_task = asyncio.create_task(process_chunk(input_queue, output_file, chunk_size))
 
-    await asyncio.gather(input_task, process_task)
+async def process_file():
+    start = time.time()
+    input_file = 'input_file'
+    output_file = 'output_file'
+    queue_size = 6
+    file_data_chunk_size = 10000000
+    input_queue = asyncio.Queue(maxsize=queue_size)
+    output_queue = asyncio.Queue(maxsize=queue_size)
 
-asyncio.run(main())
+    input_task = asyncio.create_task(read_input_file(input_file, input_queue, file_data_chunk_size))
+    mediator_task = asyncio.create_task(processor(input_queue, output_queue))
+    write_task = asyncio.create_task(write_output_file(output_queue, output_file))
+
+    await asyncio.gather(input_task, mediator_task, write_task)
+
+    end = time.time() - start
+    print("Time difference:", end, "seconds")
+
+
+if __name__ == "__main__":
+    asyncio.run(process_file())
